837258bc46e3cd578605472c722974c0eda34b52
Daniel <theendercraftgaming@gmail.com>
2020-10-21 13:47:33 -0400

    base Class Book

--- ProjectSession/app/src/main/java/com/example/projectsession/AppBundle/Book.java
+++ ProjectSession/app/src/main/java/com/example/projectsession/AppBundle/Book.java
@@ -5,4 +5,61 @@ public class Book {
     String title, actor, category, description;
     boolean available;
     Double price;
+
+    public Book(String title, String actor, String category, String description, boolean available, Double price) {
+        this.title = title;
+        this.actor = actor;
+        this.category = category;
+        this.description = description;
+        this.available = available;
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getActor() {
+        return actor;
+    }
+
+    public void setActor(String actor) {
+        this.actor = actor;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
 }
